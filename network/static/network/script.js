0aac12554c232b2f6e0ff53966fc1afd448713ae
NDRBAX <andre.bacaliu@icloud.com>
2022-10-06 20:29:33 +0200

    Reload with js works fine now

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -23,14 +23,34 @@ document.addEventListener("DOMContentLoaded", () => {
       clear();
       const section = link.dataset.section;
       showSection(section);
-      history.pushState({ section }, section, section);
+      history.pushState({ section }, section, "#" + section);
     };
   });
 
-  document.querySelector("#new-post-form").onsubmit = new_post;
+  document.querySelector("#new-post-form").onsubmit = post_button;
 
-  showSection("all-posts-section");
+  redirect();
 });
+
+function redirect() {
+  const url = window.location.href;
+  const section = url.split("#")[1];
+
+  if (!url.includes("#")) {
+    showSection("all-posts-section");
+  } else {
+    if (section.includes("profile")) {
+      showSection("profile-section");
+      console.log("profile");
+    } else if (section.includes("following")) {
+      showSection("following-section");
+      console.log("following");
+    } else if (section.includes("all-posts")) {
+      showSection("all-posts-section");
+      console.log("all-posts");
+    }
+  }
+}
 
 // Show the section with the given name
 function showSection(section) {
@@ -106,11 +126,12 @@ function fetch_posts(name) {
       });
   } else {
     document.querySelector("#new-post-section").style.display = "none";
+    document.querySelector("#pagination").style.display = "none";
   }
 }
 
 // make a new post
-function new_post(event, section) {
+function new_post(event) {
   event.preventDefault();
 
   const content = document.querySelector("#new-post-content").value;
@@ -132,7 +153,42 @@ function new_post(event, section) {
         document.querySelector("#show-alert").innerHTML = result["message"];
         console.log(content);
       }
-      // fetch posts again
-      fetch_posts("all-posts-section");
     });
 }
+
+// post button
+function post_button(event, section) {
+  new_post(event);
+  clear();
+
+  if (section === "profile-section") {
+    showSection("profile-section");
+  } else {
+    showSection("all-posts-section");
+  }
+  history.pushState({ section }, section, section);
+}
+
+// // pagination for displaying posts : 10 posts per page
+// function pagination() {
+//   const posts = document.querySelectorAll("#post");
+//   const posts_per_page = 10;
+//   const page_count = Math.ceil(posts.length / posts_per_page);
+
+//   for (let i = 0; i < page_count; i++) {
+//     const page = document.createElement("div");
+//     page.className = "page";
+//     page.id = "page-" + i;
+
+//     for (let j = 0; j < posts_per_page; j++) {
+//       const index = i * posts_per_page + j;
+//       if (index < posts.length) {
+//         page.appendChild(posts[index]);
+//       }
+//     }
+
+//     document.querySelector("#all-posts-section").appendChild(page);
+//   }
+
+//   document.querySelector("#page-0").style.display = " block";
+// }
