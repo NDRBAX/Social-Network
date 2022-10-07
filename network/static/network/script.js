4d7032d8cebcd300a288e3b8483dbe3442248cd1
NDRBAX <andre.bacaliu@icloud.com>
2022-10-07 18:04:02 +0200

    Pagination with js added

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -11,6 +11,7 @@ function clear() {
     document.querySelectorAll("#post").forEach((post) => {
       post.remove();
     });
+    console.log("cleared");
     document.querySelector("#show-alert").style.display = "none";
   } catch (error) {
     console.log(error);
@@ -29,10 +30,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelector("#new-post-form").onsubmit = post_button;
 
-  redirect();
+  refresh();
 });
 
-function redirect() {
+function refresh() {
   const url = window.location.href;
   const section = url.split("#")[1];
 
@@ -41,13 +42,10 @@ function redirect() {
   } else {
     if (section.includes("profile")) {
       showSection("profile-section");
-      console.log("profile");
-    } else if (section.includes("following")) {
-      showSection("following-section");
-      console.log("following");
     } else if (section.includes("all-posts")) {
       showSection("all-posts-section");
-      console.log("all-posts");
+    } else if (section.includes("following")) {
+      showSection("following-section");
     }
   }
 }
@@ -55,17 +53,73 @@ function redirect() {
 // Show the section with the given name
 function showSection(section) {
   try {
-    sections = ["profile-section", "following-section", "all-posts-section"];
+    let sections = [
+      "profile-section",
+      "following-section",
+      "all-posts-section",
+    ];
 
-    sections.forEach((p) => {
-      if (p !== section) {
-        document.querySelector(`#${p}`).style.display = "none";
+    sections.forEach((page) => {
+      if (page !== section) {
+        document.querySelector(`#${page}`).style.display = "none";
       } else {
-        document.querySelector(`#${p}`).style.display = "block";
+        document.querySelector(`#${page}`).style.display = "block";
+        fetch_posts(section);
       }
     });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-    fetch_posts(section);
+// Show posts
+function show_posts(posts, page, posts_per_page, name) {
+  try {
+    const start = (page - 1) * posts_per_page;
+    const end = start + posts_per_page;
+
+    posts.slice(start, end).forEach((post) => {
+      const post_div = document.createElement("div");
+      post_div.className = "post";
+      post_div.id = "post";
+
+      // POST HEADER
+      const post_header = document.createElement("div");
+      post_header.className = "post-header";
+      post_header.innerHTML = `<div class="post-user">${post.user}</div>
+      <div class="post-timestamp">${post.timestamp}</div>`;
+      post_div.append(post_header);
+
+      // POST CONTENT
+      const post_content = document.createElement("div");
+      post_content.className = "post-content";
+      post_content.innerHTML = `<div class="post-text">${post.content}</div>
+      <hr>`;
+      post_div.append(post_content);
+
+      // // POST LIKES
+      // const post_likes = document.createElement("div");
+      // post_likes.className = "post-likes";
+      // post_likes.innerHTML = `<div class="post-likes">${post.likes} likes</div>`;
+      // post_div.append(post_likes);
+
+      // // POST BUTTONS
+      // const post_buttons = document.createElement("div");
+      // post_buttons.className = "post-buttons";
+      // post_buttons.innerHTML = `<button class="btn btn-primary" id="like-button" onclick="like_button('${post.id}')">Like</button>
+      // <button class="btn btn-primary" id="edit-button" onclick="edit_button('${post.id}')">Edit</button>
+      // <button class="btn btn-primary" id="delete-button" onclick="delete_button('${post.id}')">Delete</button>`;
+      // post_div.append(post_buttons);
+
+      // // POST EDIT
+      // const post_edit = document.createElement("div");
+      // post_edit.className = "post-edit";
+      // post_edit.innerHTML = `<textarea class="form-control" id="edit-text" rows="3" placeholder="Edit your post here..."></textarea>
+      // <button class="btn btn-primary" id="edit-button" onclick="edit_button('${post.id}')">Edit</button>`;
+      // post_div.append(post_edit);
+
+      document.querySelector("#posts").append(post_div);
+    });
   } catch (error) {
     console.log(error);
   }
@@ -82,24 +136,62 @@ function fetch_posts(name) {
         if (posts.length === 0) {
           document.querySelector(`#${name}`).innerHTML = "No posts to show";
         } else {
-          posts.forEach((post) => {
-            const post_element = document.createElement("div");
-            post_element.setAttribute("id", "post");
+          document.querySelector(`#${name}`).innerHTML = `
+          <div class="row">
+            <div class="col-12">
+              <div id="posts"></div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1">
+              <button class="btn btn-primary" id="previous">Previous</button>
+            </div>
+            <div class="col-10 text-center">
+              <p id="page-number"></p>
+            </div>
+            <div class="col-1">
+              <button class="btn btn-primary" id="next">Next</button>
+            </div>
+          </div>`;
 
-            const ul = document.createElement("ul");
-            ul.className = "list-group";
-            post_element.appendChild(ul);
+          let page = 1;
+          let posts_per_page = 10;
+          let number_of_pages = Math.ceil(posts.length / posts_per_page);
 
-            const li = document.createElement("li");
-            li.className = "list-group-item mb-1";
-            li.innerHTML = post.content;
-            ul.appendChild(li);
+          show_posts(posts, page, posts_per_page, name);
 
-            document.querySelector(`#${name}`).append(post_element);
-          });
+          // show page number
+          document.querySelector(
+            "#page-number"
+          ).innerHTML = `Page ${page} of ${number_of_pages}`;
+
+          // next button
+          document.querySelector("#next").onclick = () => {
+            if (page < number_of_pages) {
+              page += 1;
+              document.querySelector("#posts").innerHTML = "";
+              show_posts(posts, page, posts_per_page, name);
+              document.querySelector(
+                "#page-number"
+              ).innerHTML = `Page ${page} of ${number_of_pages}`;
+            }
+          };
+
+          // previous button
+          document.querySelector("#previous").onclick = () => {
+            if (page > 1) {
+              page -= 1;
+              document.querySelector("#posts").innerHTML = "";
+              show_posts(posts, page, posts_per_page, name);
+              document.querySelector(
+                "#page-number"
+              ).innerHTML = `Page ${page} of ${number_of_pages}`;
+            }
+          };
         }
       });
   } else if (name === "following-section") {
+    document.querySelector("#new-post-section").style.display = "none";
     fetch(`/network/${name}`)
       .then((response) => response.json())
       .then((users) => {
@@ -112,7 +204,7 @@ function fetch_posts(name) {
             user_element.setAttribute("id", "user");
 
             const ul = document.createElement("ul");
-            ul.className = "list-group";
+            ul.className = "list-group page-list";
             user_element.appendChild(ul);
 
             const li = document.createElement("li");
@@ -124,9 +216,6 @@ function fetch_posts(name) {
           });
         }
       });
-  } else {
-    document.querySelector("#new-post-section").style.display = "none";
-    document.querySelector("#pagination").style.display = "none";
   }
 }
 
@@ -168,27 +257,3 @@ function post_button(event, section) {
   }
   history.pushState({ section }, section, section);
 }
-
-// // pagination for displaying posts : 10 posts per page
-// function pagination() {
-//   const posts = document.querySelectorAll("#post");
-//   const posts_per_page = 10;
-//   const page_count = Math.ceil(posts.length / posts_per_page);
-
-//   for (let i = 0; i < page_count; i++) {
-//     const page = document.createElement("div");
-//     page.className = "page";
-//     page.id = "page-" + i;
-
-//     for (let j = 0; j < posts_per_page; j++) {
-//       const index = i * posts_per_page + j;
-//       if (index < posts.length) {
-//         page.appendChild(posts[index]);
-//       }
-//     }
-
-//     document.querySelector("#all-posts-section").appendChild(page);
-//   }
-
-//   document.querySelector("#page-0").style.display = " block";
-// }
