57befded8fef255031ce630e757eeda5f7d9acee
NDRBAX <andre.bacaliu@icloud.com>
2022-10-11 18:07:33 +0200

    Minor fixes about navigation between pages

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -21,10 +21,13 @@ function clear() {
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("a").forEach((link) => {
     link.onclick = () => {
-      clear();
+      location.reload();
+      console.log("clicked");
       const section = link.dataset.section;
-      showSection(section);
-      history.pushState({ section }, section, "#" + section);
+      if (section) {
+        showSection(section);
+        history.pushState({ section }, section, "#" + section);
+      }
     };
   });
 
@@ -35,9 +38,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function refresh() {
   const url = window.location.href;
+  console.log(url);
   const section = url.split("#")[1];
-
-  if (!url.includes("#")) {
+  console.log("refreshed");
+  if (section === undefined) {
     showSection("all-posts-section");
   } else {
     if (section.includes("profile")) {
@@ -72,127 +76,102 @@ function showSection(section) {
   }
 }
 
-// Show posts
-function show_posts(posts, page, posts_per_page, name) {
+function show_posts(posts, page, posts_per_page) {
   try {
-    const start = (page - 1) * posts_per_page;
-    const end = start + posts_per_page;
+    let start = (page - 1) * posts_per_page;
+    let end = page * posts_per_page;
 
     posts.slice(start, end).forEach((post) => {
-      const post_div = document.createElement("div");
-      post_div.className = "post";
-      post_div.id = "post";
+      const post_element = document.createElement("div");
+      post_element.setAttribute("id", "post");
+      post_element.className = "card mb-2";
 
-      // POST HEADER
       const post_header = document.createElement("div");
-      post_header.className = "post-header";
-      post_header.innerHTML = `<div class="post-user">${post.user}</div>
-      <div class="post-timestamp">${post.timestamp}</div>`;
-      post_div.append(post_header);
+      post_header.className = "card-header";
+      post_header.innerHTML = post.user;
+      post_element.appendChild(post_header);
 
-      // POST CONTENT
-      const post_content = document.createElement("div");
-      post_content.className = "post-content";
-      post_content.innerHTML = `<div class="post-text">${post.content}</div>
-      <hr>`;
-      post_div.append(post_content);
+      const post_body = document.createElement("div");
+      post_body.className = "card-body";
+      post_body.innerHTML = post.content;
+      post_element.appendChild(post_body);
 
-      // // POST LIKES
-      // const post_likes = document.createElement("div");
-      // post_likes.className = "post-likes";
-      // post_likes.innerHTML = `<div class="post-likes">${post.likes} likes</div>`;
-      // post_div.append(post_likes);
+      const post_footer = document.createElement("div");
+      post_footer.className = "card-footer";
+      post_footer.innerHTML = post.timestamp;
+      post_element.appendChild(post_footer);
 
-      // // POST BUTTONS
-      // const post_buttons = document.createElement("div");
-      // post_buttons.className = "post-buttons";
-      // post_buttons.innerHTML = `<button class="btn btn-primary" id="like-button" onclick="like_button('${post.id}')">Like</button>
-      // <button class="btn btn-primary" id="edit-button" onclick="edit_button('${post.id}')">Edit</button>
-      // <button class="btn btn-primary" id="delete-button" onclick="delete_button('${post.id}')">Delete</button>`;
-      // post_div.append(post_buttons);
-
-      // // POST EDIT
-      // const post_edit = document.createElement("div");
-      // post_edit.className = "post-edit";
-      // post_edit.innerHTML = `<textarea class="form-control" id="edit-text" rows="3" placeholder="Edit your post here..."></textarea>
-      // <button class="btn btn-primary" id="edit-button" onclick="edit_button('${post.id}')">Edit</button>`;
-      // post_div.append(post_edit);
-
-      document.querySelector("#posts").append(post_div);
+      document.querySelector("#posts").append(post_element);
     });
   } catch (error) {
     console.log(error);
   }
 }
 
-// fetch posts
-function fetch_posts(name) {
+async function fetch_posts(name) {
   if (name === "all-posts-section" || name === "profile-section") {
     document.querySelector("#new-post-section").style.display = "block";
 
-    fetch(`/network/${name}`)
+    await fetch(`/network/${name}`)
       .then((response) => response.json())
       .then((posts) => {
         if (posts.length === 0) {
           document.querySelector(`#${name}`).innerHTML = "No posts to show";
         } else {
-          document.querySelector(`#${name}`).innerHTML = `
-          <div class="row">
-            <div class="col-12">
-              <div id="posts"></div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-1">
-              <button class="btn btn-primary" id="previous">Previous</button>
-            </div>
-            <div class="col-10 text-center">
-              <p id="page-number"></p>
-            </div>
-            <div class="col-1">
-              <button class="btn btn-primary" id="next">Next</button>
-            </div>
-          </div>`;
-
           let page = 1;
           let posts_per_page = 10;
-          let number_of_pages = Math.ceil(posts.length / posts_per_page);
+          let total_pages = Math.ceil(posts.length / posts_per_page);
 
-          show_posts(posts, page, posts_per_page, name);
+          document.querySelector(`#${name}`).innerHTML = `
+            <div class="row mt-5">
+                <div class="col-12">
+                  <div id="posts"></div>
+                </div>
+              </div>
+              <div class="row mt-5 mb-5">
+                <div class="col-1">
+                  <button class="btn btn-primary" id="previous">Previous</button>
+                </div>
+                <div class="col-10 text-center">
+                  <p id="page-number"></p>
+                </div>
+                <div class="col-1">
+                  <button class="btn btn-primary" id="next">Next</button>
+                </div>
+            </div>`;
 
-          // show page number
+          show_posts(posts, page, posts_per_page);
+
           document.querySelector(
             "#page-number"
-          ).innerHTML = `Page ${page} of ${number_of_pages}`;
+          ).innerHTML = `${page} of ${total_pages}`;
 
-          // next button
           document.querySelector("#next").onclick = () => {
-            if (page < number_of_pages) {
-              page += 1;
-              document.querySelector("#posts").innerHTML = "";
-              show_posts(posts, page, posts_per_page, name);
+            if (page < total_pages) {
+              page++;
+              clear();
+              show_posts(posts, page, posts_per_page);
               document.querySelector(
                 "#page-number"
-              ).innerHTML = `Page ${page} of ${number_of_pages}`;
+              ).innerHTML = `${page} of ${total_pages}`;
             }
           };
 
-          // previous button
           document.querySelector("#previous").onclick = () => {
             if (page > 1) {
-              page -= 1;
-              document.querySelector("#posts").innerHTML = "";
-              show_posts(posts, page, posts_per_page, name);
+              page--;
+              clear();
+              show_posts(posts, page, posts_per_page);
               document.querySelector(
                 "#page-number"
-              ).innerHTML = `Page ${page} of ${number_of_pages}`;
+              ).innerHTML = `${page} of ${total_pages}`;
             }
           };
         }
       });
   } else if (name === "following-section") {
     document.querySelector("#new-post-section").style.display = "none";
-    fetch(`/network/${name}`)
+    await fetch(`/network/${name}`)
       .then((response) => response.json())
       .then((users) => {
         if (users.length === 0) {
@@ -209,7 +188,7 @@ function fetch_posts(name) {
 
             const li = document.createElement("li");
             li.className = "list-group-item mb-1";
-            li.innerHTML = user;
+            li.innerHTML = user.posts;
             ul.appendChild(li);
 
             document.querySelector(`#${name}`).append(user_element);
