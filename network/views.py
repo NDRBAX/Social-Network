4b7f2b71dd85467fd8dad19e10dd8ee5391408cb
NDRBAX <andre.bacaliu@icloud.com>
2022-10-17 17:44:58 +0200

    Show user profile view added

--- network/views.py
+++ network/views.py
@@ -13,8 +13,27 @@ from .models import *
 def index(request):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user=request.user)
+
+        # number of user posts
+        posts = Post.objects.filter(user=request.user).order_by("-timestamp").all()
+        num_posts = len(posts)
+
+        # number of user followers
+        followers = profile.followers.all()
+        num_followers = len(followers)
+
+        # number of user following
+        following = profile.following.all()
+        num_following = len(following)
+
+        print(profile.user)
+
         return render(request, "network/index.html", {
-            "profile": profile
+            "profile": profile,
+            "num_posts": num_posts,
+            "num_followers": num_followers,
+            "num_following": num_following
+
         })
     else:
         return HttpResponseRedirect(reverse("login"))
@@ -80,9 +99,9 @@ def section(request, section):
     elif section == "profile-section":
         posts = Post.objects.filter(user=request.user)
     elif section == "following-section":
-        following = Follow.objects.get(follower=request.user).following.all()
-        posts = Post.objects.filter(user__in=following) 
-        return JsonResponse([following.serialize() for following in following], safe=False)
+        following = Profile.objects.get(user=request.user).following.all()
+        posts = Post.objects.filter(user__in=following)
+
     else:
         return JsonResponse({"error": "Invalid page."}, status=400)
 
@@ -99,14 +118,27 @@ def edit_profile(request):
         return JsonResponse({"error": "PUT request required."}, status=400)
 
     data = json.loads(request.body)
-    if "avatar" not in data or "country" not in data or "age" not in data or "bio" not in data:
+
+    if "avatar" not in data or "country" not in data or "age" not in data or "bio" not in data or "background_cover" not in data:
         return JsonResponse({"error": "Missing fields."}, status=400)
 
+    print("covert", data["background_cover"])
+  
     profile = Profile.objects.get(user=request.user)
     profile.avatar = data["avatar"]
+    profile.background_cover = data["background_cover"]
     profile.country = data["country"]
     profile.age = data["age"]
     profile.bio = data["bio"]
+    
+
+    # get all the posts of this user and update the avatar
+    posts = Post.objects.filter(user=request.user)
+    print(posts)
+    for post in posts:
+        post.user_avatar = data["avatar"]
+        post.save()
+
     profile.save()
 
     return JsonResponse({"message": "Profile updated successfully."}, status=201)
@@ -172,3 +204,22 @@ def follow_user(request, username):
     return JsonResponse({"error": "PUT request required."}, status=400)
 
 
+# show user profile 
+@login_required
+def user_profile(request, username):
+    if request.method == "GET":
+        user = User.objects.get(username=username)
+        profile = Profile.objects.get(user=user)
+        posts = Post.objects.filter(user=user).all().order_by("-timestamp")
+        print(posts)
+        return JsonResponse({
+            "user": user.username, 
+            "avatar": profile.avatar, 
+            "background_cover": profile.background_cover, 
+            "country": profile.country, 
+            "age": profile.age, 
+            "bio": profile.bio,
+            "followers": profile.followers.count(),
+            "following": profile.following.count(),
+            "joined": user.date_joined,
+            "posts": [post.serialize() for post in posts]}, safe=False, status=201)
