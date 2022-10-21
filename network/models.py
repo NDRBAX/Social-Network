51ad8d2d16808a5b9136720940518a9d646c0fe4
NDRBAX <andre.bacaliu@icloud.com>
2022-10-21 20:41:52 +0200

    Comment modele created

--- network/models.py
+++ network/models.py
@@ -6,7 +6,6 @@ class User(AbstractUser):
     pass
 
 class Profile(models.Model):
-    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profile")
     background_cover = models.CharField(max_length=1000, default="https://images.unsplash.com/photo-1664199134378-459f80ded70c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1216&q=80")
     avatar = models.CharField(max_length=500, default="https://cdn-icons-png.flaticon.com/512/3177/3177440.png",)    
@@ -33,14 +32,17 @@ class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
     post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="post")
 
-    def __str__(self):
-        return f"{self.user} likes {self.post}"
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "post": self.post.id
+        }
 
 class Post(models.Model):
-    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, blank=True, on_delete=models.CASCADE, related_name="posts")
     user_avatar = models.CharField(max_length=500, default="https://cdn-icons-png.flaticon.com/512/3177/3177440.png",)
-    content = models.CharField(max_length=280)
+    content = models.CharField(max_length=300)
     timestamp = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
 
@@ -52,4 +54,19 @@ class Post(models.Model):
             'user_avatar': self.user_avatar,
             "timestamp": self.timestamp,
             "likes": self.likes
+        }
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    content = models.CharField(max_length=280)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "post": self.post.id,
+            "content": self.content,
+            "timestamp": self.timestamp
         }
