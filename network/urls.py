775bc0e3aeb0c9f08b36de9323e2163ba58d0e28
NDRBAX <andre.bacaliu@icloud.com>
2022-10-24 17:56:34 +0200

    Minor fixes

--- network/urls.py
+++ network/urls.py
@@ -8,26 +8,15 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-
+    
     # API 
     path("network/<str:section>", views.section, name="section"),
     path("new-post", views.new_post, name="new-post"),
     path("edit-post/<int:post_id>", views.edit_post, name="edit-post"),
     path("like-post/<int:post_id>", views.like_post, name="like-post"),
     path("follow-user/<str:username>", views.follow_user, name="follow-user"),
-    # edit profile
     path("edit-profile", views.edit_profile, name="edit-profile"),
-    # profile
     path("profile/<str:username>", views.user_profile, name="profile"),
-    # comments
     path("comments/<int:post_id>", views.new_comment, name="comments"),
-    # delete post
-    path("delete-post/<int:post_id>", views.delete_post, name="delete-post"),
-
-
-
-    
-
-
-
+    path("delete-post/<int:post_id>", views.delete_post, name="delete-post")
 ]
